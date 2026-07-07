36a5e833ef80c07753bb1e68be40162771b30777
Ejie Florida <programmerejie@gmail.com>
2026-07-07 12:20:06 +0800

    application: setup files

---
+++ anibenta-app/app/index.jsx
@@ -0,0 +1,26 @@
+import {View,Text,StyleSheet} from "react-native";
+
+const Home = () => {
+    return(
+        <View style={styles.container}>
+            <Text style={styles.text}>Welcome to AniBenta Farm Management System</Text>
+        </View>
+
+    )
+}
+
+export default Home;
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 18,
+        fontWeight: '600',
+        textAlign: 'center',
+    },
+})
